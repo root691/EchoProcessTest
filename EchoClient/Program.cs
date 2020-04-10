c41099feae53c11dbca30c8fdfd7da0c9e4e118f
root691 <>
2020-04-10 22:03:55 +0300

    Add test client and server

---
+++ EchoClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Echo;
+using LanguageExt;
+using static Echo.Process;
+
+namespace EchoClient
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            RedisCluster.register();
+            ProcessConfig.initialise("test-cluster", "client", "client-1", "localhost", "0");
+
+            Console.Write("Waiting...");
+            Console.ReadLine();
+
+            ProcessId server = "//test-cluster/node-1/user/logger";
+
+            var t = spawn<Unit>("client", _ => tell(server, "Hello World!"));
+
+            while (true)
+            {
+                tell(t, Unit.Default);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ EchoServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Echo;
+using LanguageExt;
+using static Echo.Process;
+
+namespace EchoServer
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            RedisCluster.register();
+            ProcessConfig.initialise("test-cluster", "main-server", "node-1", "localhost", "0");
+
+            Console.Write("Waiting...");
+            Console.ReadLine();
+
+            ProcessId client = "//test-cluster/client-1/user/client-1";
+
+            var t = spawn<Unit, string>("logger",
+                () => subscribe(client),
+                (unit, message) =>
+                {
+                    Console.WriteLine($"{message} - {DateTime.Now:HH:mm:ss.fff}");
+                    return Unit.Default;
+                });
+
+            //register("logger", t);
+            tell(t, "Start");
+
+            Console.ReadLine();
+        }
+    }
+}
