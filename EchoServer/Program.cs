9149a2beb5e7a4dbd511d1ef46a12b30bd162085
harrhp <>
2020-04-12 01:52:36 +0300

    добÐadded persistence. log actor environment

--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Threading;
 using Echo;
-using LanguageExt;
 using static Echo.Process;
 
 namespace EchoServer
@@ -13,21 +11,50 @@ namespace EchoServer
             RedisCluster.register();
             ProcessConfig.initialise("test-cluster", "main-server", "node-1", "localhost", "0");
 
-            Console.Write("Waiting...");
-            Console.ReadLine();
+            ProcessSystemLog.Subscribe(Console.WriteLine);
 
-            ProcessId client = "//test-cluster/client-1/user/client-1";
-
-            var t = spawn<Unit, string>("logger",
-                () => subscribe(client),
-                (unit, message) =>
+            var logger = spawn<int, string>(
+                "logger",
+                () => 0,
+                (count, message) =>
                 {
+                    Console.WriteLine("--- MESSAGE RECEIVED ---");
                     Console.WriteLine($"{message} - {DateTime.Now:HH:mm:ss.fff}");
-                    return Unit.Default;
-                });
+                    Console.WriteLine("--- MESSAGE HANDLED ---");
+                    Console.WriteLine("--- META INFO START ---");
+                    Console.WriteLine(Self);
+                    Console.WriteLine(Parent);
+                    Console.WriteLine(Systems);
+                    Console.WriteLine(Children);
+                    Console.WriteLine(Sender);
+                    Console.WriteLine(isAsk);
+                    Console.WriteLine(isTell);
+                    Console.WriteLine(inboxCount(Self));
+                    Console.WriteLine(Role.NodeIds(Self).ToSeq());
+                    Console.WriteLine(Role.NodeIds(Sender).ToSeq());
+                    foreach (var systemName in Systems)
+                    {
+                        Console.WriteLine(DeadLetters(systemName));
+                        Console.WriteLine(Errors(systemName));
+                        Console.WriteLine(Root(systemName));
+                        Console.WriteLine(User(systemName));
+                        Console.WriteLine(getSupplementarySessionId());
+                        Console.WriteLine(hasActiveSession());
+                        Console.WriteLine(hasSession());
+                        Console.WriteLine(ClusterNodes(systemName).Map(cn => (cn.Role, cn.NodeName, cn.LastHeartbeat)));
+                        Console.WriteLine(Role.Nodes(Self, systemName));
+                        Console.WriteLine(Role.Nodes(Sender, systemName));
+                    }
 
-            //register("logger", t);
-            tell(t, "Start");
+                    Console.WriteLine("--- META INFO END ---");
+
+                    return count + 1;
+                },
+                ProcessFlags.PersistAll);
+
+            register("logger", logger);
+
+            Console.WriteLine(logger);
 
             Console.ReadLine();
         }
